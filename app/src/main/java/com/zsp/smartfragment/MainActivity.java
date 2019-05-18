accf55316bb8e58c3785787b1bd171359657a37f
snpmyn <snpmyn@126.com>
2019-05-18 21:10:39 +0800

    Initial commit

---
+++ app/src/main/java/com/zsp/smartfragment/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zsp.smartfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @decs: 主页
+ * @author: 郑少鹏
+ * @date: 2019/5/18 20:27
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
