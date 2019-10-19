6930fa6a4c200eaaf15e5092d3766293c80ef9b8
snpmyn <snpmyn@126.com>
2019-10-19 17:22:35 +0800

    优化

--- app/src/main/java/application/SmartFragmentApp.java
+++ app/src/main/java/application/SmartFragmentApp.java
@@ -1,8 +1,13 @@
 package application;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 
 import com.zsp.smartfragment.BuildConfig;
+import com.zsp.utilone.activity.ActivitySuperviseManager;
 import com.zsp.utilone.timber.configure.TimberInitConfigure;
 
 /**
@@ -25,7 +30,53 @@ public class SmartFragmentApp extends Application {
     private void initConfiguration() {
         // timber
         TimberInitConfigure.initTimber(BuildConfig.DEBUG);
+        // 全局监听Activity生命周期
+        registerActivityListener();
         // fragmentation
         FragmentationInitConfig.initFragmentation();
+    }
+
+    /**
+     * Activity全局监听
+     */
+    private void registerActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
+                // 添监听到创事件Activity至集合
+                ActivitySuperviseManager.pushActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+                // 移监听到销事件Activity出集合
+                ActivitySuperviseManager.removeActivity(activity);
+            }
+        });
     }
 }
